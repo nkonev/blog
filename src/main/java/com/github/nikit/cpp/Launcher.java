08edf8b42924e00f1a77e0711ac54734ef85d89c
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-05-20 03:49:28 +0300

    first commit

---
+++ src/main/java/com/github/nikit/cpp/Launcher.java
@@ -0,0 +1,10 @@
+package com.github.nikit.cpp;
+
+/**
+ * Created by nik on 20.05.17.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]");
+    }
+}
