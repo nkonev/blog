90f502517cb26ddceb1195b32e893f4e7fb9cb6b
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-06-06 02:38:41 +0300

    override selenium dependencies and initial failsafe plugin for IT

---
+++ integration-test/src/test/java/com/github/FirstIT.java
@@ -0,0 +1,14 @@
+package com.github;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by nik on 06.06.17.
+ */
+public class FirstIT {
+    @Test
+    public void testOne() throws Exception {
+        Assert.assertTrue("tr", true);
+    }
+}
