b65b4f00ee8efda132bc091765e30b665876b355
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-06-24 18:36:36 +0300

    vue-offline

--- frontend/src/main/frontend/App.vue
+++ frontend/src/main/frontend/App.vue
@@ -1,39 +1,51 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <nav>
-        <router-link to="/login">Login</router-link>
-        <router-link to="/">Index</router-link>
-        <router-link to="/users">Users</router-link>
-        <router-link id="a-api" to="/helloween">helloween</router-link>
-        <router-link to="/not/found">Not found</router-link>
-        <router-link to="/autocomplete">Autocomplete</router-link>
-        <a id="a-doc" href="/docs/index.html">Docs</a>
-    </nav>
-    <router-view></router-view>
+    <div id="app">
+        <OfflineIndicator message="Oh no, you're offline :("></OfflineIndicator>
+
+        <img src="./assets/logo.png">
+        <nav>
+            <router-link to="/login">Login</router-link>
+            <router-link to="/">Index</router-link>
+            <router-link to="/users">Users</router-link>
+            <router-link id="a-api" to="/helloween">helloween</router-link>
+            <router-link to="/not/found">Not found</router-link>
+            <router-link to="/autocomplete">Autocomplete</router-link>
+            <a id="a-doc" href="/docs/index.html">Docs</a>
+        </nav>
+        <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    import Vue from 'vue';
+    import { OfflineIndicator } from 'vue-online';
+    export default {
+        name: 'app',
+        components: {
+            OfflineIndicator
+        }
+    }
 </script>
 
 <style lang="stylus">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
 
-  nav {
-      a.router-link-exact-active {
-          color white
-          background-color #003eff
+      nav {
+          a.router-link-exact-active {
+              color white
+              background-color #003eff
+          }
       }
-  }
-}
+    }
+
+    div.offline-indicator {
+        background: #ff6762;
+        color: white;
+    }
 </style>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -69,6 +69,7 @@ module.exports = {
     resolve: {
         alias: {
             'jquery': require.resolve('jquery'), // for uniform.js
+            'vue': 'vue/dist/vue.js', // fix "Vue is not constructor" in vue-online
             'vue$': 'vue/dist/vue.esm.js', // it's important, else you will get "You are using the runtime-only build of Vue where the template compiler is not available. Either pre-compile the templates into render functions, or use the compiler-included build."
         }
     },
