999cada77e1e2099c75d98d8fcd4819ece746444
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-07-03 17:59:42 +0300

    v-autocomplete

--- frontend/src/main/frontend/components/Autocomplete.vue
+++ frontend/src/main/frontend/components/Autocomplete.vue
@@ -2,57 +2,106 @@
     <div>
         <h1>Type some country</h1>
 
-        <vue-instant
-                :suggestion-attribute="suggestionAttribute"
-                v-model="value"
-                :disabled="false"
-                @input="changed"
-                :show-autocomplete="true"
-                :autofocus="false"
-                :suggestions="suggestions"
-                name="customName"
-                placeholder="custom placeholder"
-                type="google"
-        ></vue-instant>
+        <template>
+            <v-autocomplete :items="items" v-model="item" :get-label="getLabel" :component-item='template' @change="fetchItems">
+            </v-autocomplete>
+        </template>
     </div>
 </template>
 
 <script>
     import Vue from 'vue'
-    import 'vue-instant/dist/vue-instant.css'
-    // import VueInstant from 'vue-instant/dist/vue-instant.common.js'
-    import VueInstant from 'vue-instant'
-    Vue.use(VueInstant);
+    import Autocomplete from 'v-autocomplete'
+    import 'v-autocomplete/dist/v-autocomplete.css'
 
+    Vue.use(Autocomplete);
+    import ItemTemplate from './AutocompleteItemTemplate.vue'
     export default {
-        name: "Autocomplete",
-        data() {
+        data () {
             return {
-                value: '',
-                suggestionAttribute: 'value', // AutocompleteResponse
-                suggestions: [],
+                item: {id: 0, value: 'None', description: 'Please type'},
+                template: ItemTemplate,
+                items: [],
             }
         },
         methods: {
-            changed: function() {
-                var that = this;
-                this.suggestions = [];
-                this.$http.get('/api/public/autocomplete?prefix=' + this.value)
+            getLabel (item) {
+                return item.value
+            },
+            fetchItems (text) {
+                console.log('fetching items for', text);
+                const that = this;
+                this.$http.get('/api/public/autocomplete?prefix=' + text)
                     .then(
                         response => {
-                            that.suggestions = response.body;
-                            console.log(that.suggestions)
+                            that.items = response.body;
+                            console.log(that.items);
                         },
                         response => {
                             // error callback
                             console.error(response);
                         }
                     )
+
             }
         },
-        /*components: {
-            vueInstant: VueInstant
-        }*/
+        computed:{
+            items1212 () {
+                /*console.log('fetching items...', this);
+                const that = this;
+                this.$http.get('/api/public/autocomplete?prefix=' + this.value)
+                    .then(
+                        response => {
+                            that.items = response.body;
+                            console.log(that.items);
+                        },
+                        response => {
+                            // error callback
+                            console.error(response);
+                        }
+                    )*/
+            }
+        }
     }
-
 </script>
+
+<style lang="stylus">
+    .v-autocomplete
+        .v-autocomplete-input-group
+            .v-autocomplete-input
+                font-size 1.5em
+                padding 10px 15px
+                box-shadow none
+                border 1px solid #157977
+                width calc(100% - 32px)
+                outline none
+                background-color #eee
+            &.v-autocomplete-selected
+                .v-autocomplete-input
+                    color green
+                    background-color #f2fff2
+        .v-autocomplete-list
+            width 100%
+            text-align left
+            border none
+            border-top none
+            max-height 400px
+            overflow-y auto
+            border-bottom 1px solid #157977
+            .v-autocomplete-list-item
+                cursor pointer
+                background-color #fff
+                padding 10px
+                border-bottom 1px solid #157977
+                border-left 1px solid #157977
+                border-right 1px solid #157977
+                &:last-child
+                    border-bottom none
+                &:hover
+                    background-color #eee
+                abbr
+                    opacity 0.8
+                    font-size 0.8em
+                    display block
+                    font-family sans-serif
+</style>
