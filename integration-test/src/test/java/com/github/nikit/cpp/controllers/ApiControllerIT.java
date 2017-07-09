982e20f5ea8029df4aaa760dc7aaed1fe9122377
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-07-10 01:36:30 +0300

    test for twice login

--- integration-test/src/test/java/com/github/nikit/cpp/controllers/ApiControllerIT.java
+++ integration-test/src/test/java/com/github/nikit/cpp/controllers/ApiControllerIT.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 import static com.codeborne.selenide.Selenide.open;
@@ -24,22 +25,61 @@ public class ApiControllerIT extends AbstractItTestRunner {
         clearBrowserCookies();
     }
 
+    public static class LoginModal {
+        private String user, password;
+        public LoginModal(String user, String password){
+            this.user = user;
+            this.password = password;
+        }
+
+        public void callProtectedMethod() {
+            $(ID_API).click();
+        }
+
+        public void login() {
+            $("body").shouldHave(text("Пожалуйста, представьтесь"));
+            $("input#username").setValue(user);
+            $("input#password").setValue(password);
+            $(ID_SUBMIT).click();
+            $(".user-profile-nav-login").shouldHave(text(""+user));
+        }
+
+        public void logout() {
+            $(".multiselect").click();
+            $(byText("exit")).click();
+        }
+
+        public void openLoginModal() {
+            $(byText("login")).click();
+        }
+    }
+
     @Test
     public void useSeeHisLoginAfterSuccessfulLogin() throws Exception {
         open(urlPrefix+ IntegrationTestConstants.INDEX_HTML);
 
-        $(ID_API).click();
-        $("body").shouldHave(text("Пожалуйста, представьтесь"));
+        LoginModal loginModal = new LoginModal(user, password);
 
-        $("input#username").setValue(user);
-        $("input#password").setValue(password);
-        $(ID_SUBMIT).click();
+        loginModal.callProtectedMethod();
 
-        // Tests are unstable on TravisCI (It uses docker)  without this assert
-        // $(".hello").shouldHave(text("Welcome to"));
-        // open(urlPrefix+ IntegrationTestConstants.INDEX_HTML);
-        $(ID_API).click();
-
-        $(".user-profile-nav-login").shouldHave(text(""+user));
+        loginModal.login();
     }
+
+    @Test
+    public void userCanTwiceLoginLogout() throws Exception {
+        open(urlPrefix+ IntegrationTestConstants.INDEX_HTML);
+
+        LoginModal loginModal = new LoginModal(user, password);
+
+        loginModal.callProtectedMethod();
+
+        loginModal.login();
+
+        loginModal.logout();
+
+        loginModal.openLoginModal();
+
+        loginModal.login();
+    }
+
 }
