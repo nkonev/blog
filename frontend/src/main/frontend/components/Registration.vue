be3beb9c6a21a1c79d53399786163db6cf15938a
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-07-10 04:15:58 +0300

    required mark

--- frontend/src/main/frontend/components/Registration.vue
+++ frontend/src/main/frontend/components/Registration.vue
@@ -2,17 +2,17 @@
     <div class="registration" id="registration">
         <form action="/api/register" method="post">
             <div class="field">
-                <label for="login">Login</label>
+                <label for="login">Login <span class="required-mark">*</span></label>
                 <input id="login" v-model="profile.login" name="login" v-validate="'required'" autofocus/>
                 <span class="help-block" v-show="errors.has('login')">{{ errors.first('login') }}</span>
             </div>
             <div class="field">
-                <label for="email">Email</label>
+                <label for="email">Email <span class="required-mark">*</span></label>
                 <input id="email" v-model="profile.email" name="email" v-validate="'required|email'"/>
                 <span class="help-block" v-show="errors.has('email')">{{ errors.first('email') }}</span>
             </div>
             <div class="field">
-                <label for="password">password</label>
+                <label for="password">password <span class="required-mark">*</span></label>
                 <input id="password" type="password" v-model="profile.password" name="password" v-validate="'required'"/>
                 <span class="help-block" v-show="errors.has('password')">{{ errors.first('password') }}</span>
             </div>
@@ -70,6 +70,10 @@
         display block
         position relative
         margin 10px;
+
+        .required-mark {
+            color red
+        }
 
         .field {
             margin-bottom 4px;
