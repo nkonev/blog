ce40966a1a9bd52898cfca5618b06909dd6d0204
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-07-17 03:38:25 +0300

    switched back to vue-instant on specific commit, disabled node_modules cache due npm issues

--- integration-test/src/test/java/com/github/nikit/cpp/pages/AutocompleteControllerIT.java
+++ integration-test/src/test/java/com/github/nikit/cpp/pages/AutocompleteControllerIT.java
@@ -3,6 +3,12 @@ package com.github.nikit.cpp.pages;
 import com.github.nikit.cpp.integration.AbstractItTestRunner;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -14,14 +20,27 @@ import static com.github.nikit.cpp.IntegrationTestConstants.Pages.AUTOCOMPLETE;
  */
 public class AutocompleteControllerIT extends AbstractItTestRunner {
 
-    @Ignore
+    @Autowired
+    private WebDriver driver;
+
     @Test
     public void testUni() throws Exception {
 
         open(urlPrefix+ AUTOCOMPLETE);
-        $(".countries-autocomplete-input").setValue("Uni");
-        $(".v-autocomplete-list").shouldHave(text("United Arab Emirates"));
+
+        final String SELECTOR = "input.sbx-google__input";
+
+        $(SELECTOR).getWrappedElement().sendKeys("Uni");
+
+        WebElement e = $(SELECTOR).getWrappedElement();
+
+        // root cause -- send custom KEYUP event for vue-instant (https://stackoverflow.com/questions/25012780/how-do-i-enter-text-in-textfield-with-onkeyup-onfocus-javascript/25030094#25030094)
+        // e.sendKeys("t"); // not necessary
+        new Actions(driver).keyDown(e, Keys.CONTROL).keyUp(e, Keys.CONTROL).perform();
+
+        $(".vue-instant__suggestions").shouldHave(text("United Arab Emirates"));
 
         //TimeUnit.SECONDS.sleep(9999);
     }
+
 }
