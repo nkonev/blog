cb659f6509278049c07caa0cf756563229284698
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-07-23 01:53:46 +0300

    post edit with spinner

--- frontend/src/main/frontend/components/PostEdit.vue
+++ frontend/src/main/frontend/components/PostEdit.vue
@@ -12,7 +12,10 @@
         </quill-editor>
 
         <div class="command-buttons">
-            <button class="save-btn">Сохранить</button>
+            <div class="send">
+                <spinner v-if="submitting" class="send-spinner" :line-size="10" :spacing="20" :speed="0.4" size="55" :font-size="20" message="Sending..."></spinner>
+                <button v-if="!submitting" class="save-btn" @click="onBtnSave" v-bind:disabled="hasInvalidText()">Сохранить</button>
+            </div>
         </div>
     </div>
 </template>
@@ -20,31 +23,79 @@
 <script>
     import Vue from 'vue'
     import VueQuillEditor from 'vue-quill-editor'
-
     import { quillEditor } from 'vue-quill-editor'
+    import Spinner from 'vue-simple-spinner'  // https://github.com/dzwillia/vue-simple-spinner/blob/master/examples-src/App.vue
+
+    const MIN_LENGTH = 10;
+
+    function strip(html) {
+        const tmp = document.implementation.createHTMLDocument("New").body;
+        tmp.innerHTML = html;
+        return tmp.textContent || tmp.innerText || "";
+    }
+
+    // https://quilljs.com/docs/modules/toolbar/
+    const toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['link', 'image'],
+        ['blockquote', 'code-block'],
+
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'header': [1, 2, 3, 4, false] }],
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+
+        ['clean']                                         // remove formatting button
+    ];
 
     export default {
         data () {
             return {
-                content: '<h2>I am Example</h2>',
+                submitting: false,
+                content: 'Input something',
                 editorOption: {
-                    // some quill options
+                    modules: {
+                        toolbar: toolbarOptions
+                    }
                 }
             }
         },
         methods: {
             onEditorBlur(editor) {
-                console.log('editor blur!', editor)
+                console.debug('editor blur!')
             },
             onEditorFocus(editor) {
-                console.log('editor focus!', editor)
+                console.debug('editor focus!')
             },
             onEditorReady(editor) {
-                console.log('editor ready!', editor)
+                console.debug('editor ready!')
+            },
+            onBtnSave() {
+                const self = this;
+                console.log('start submitting');
+                this.submitting = true;
+                const quillInstance = self.$refs.myQuillEditor.quill;
+                quillInstance.enable(false);
+                setTimeout(()=>{
+                    self.submitting = false;
+                    quillInstance.enable(true);
+                    console.log('end submitting');
+                }, 4000);
+
+            },
+            hasInvalidText() {
+                return strip(this.content).length < MIN_LENGTH;
             }
         },
         components: {
-            quillEditor
+            quillEditor,
+            Spinner
         },
         watch: {
             content:  function (val) {
@@ -60,19 +111,35 @@
 
     .command-buttons {
 
-        .save-btn {
-            height 32px
-            min-width 64px
-            border-radius 2px;
-            border-width 1px;
-            background: white;
-            color: $signin_color;
-            &:hover {
-                border-color: $signin_color;
-                background: cornflowerblue;
-                color: white;
+        .send {
+            display inline
+
+            .send-spinner {
+                margin: 2px;
+            }
+
+            .save-btn {
+                height 32px
+                min-width 64px
+                border-radius 2px;
+                border-width 1px;
+                background: white;
+                color: $signin_color;
+                &:hover:enabled {
+                    border-color: $signin_color;
+                    background: cornflowerblue;
+                    color: white;
+                }
+                &:disabled{
+                    color: red
+                }
+                &:hover:disabled {
+                    background: red;
+                    color: white;
+                }
             }
         }
+
     }
 
 </style>
