f6ec36297934708101f1bc10b2bf4a9b8a51547f
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-07-29 18:45:02 +0300

    img constraints

--- frontend/src/main/frontend/components/PostItem.vue
+++ frontend/src/main/frontend/components/PostItem.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="post">
-        <img :src="postDTO.titleImg"/>
-        <router-link :to="{name: post, params: { id: postDTO.id} }"><h2>{{postDTO.title}}</h2></router-link>
-        <div class="post-content">
-            {{postDTO.text}}
-            <hr/>
+        <div class="img-wrapper">
+            <img :src="postDTO.titleImg"/>
         </div>
+        <div class="text-wrapper">
+            <router-link :to="{name: post, params: { id: postDTO.id} }"><h2>{{postDTO.title}}</h2></router-link>
+            <div class="post-content">
+                {{postDTO.text}}
+            </div>
+        </div>
+        <hr/>
     </div>
 </template>
 
@@ -26,29 +30,46 @@
 <style lang="stylus" scoped>
     @import "../constants.styl"
     $borderColor=red;
+    $imgMaxHeight=200px
     .post {
         display block
-    // border-style solid
+        // border-style solid
         border-width 1px;
         border-color $borderColor
         padding 2px;
         margin 2px;
+        //clear: both
 
-        h2 {
-            padding 2px;
-            margin 2px;
+        .img-wrapper {
+            // max-height 400px;
+            // clear both
+            img {
+                float: left
+                margin 4px;
+                width: 250px;
+                // display inline
+                // height: auto;
+                //max-height 400px;
+                max-height $imgMaxHeight
+            }
         }
 
-        img {
-            float: left
-            margin 4px;
-            width: 250px;
-            height: auto;
-            max-height 400px;
-        }
+        .text-wrapper {
+            //clear: both
+            // height auto
+            // float: right;
+            min-height $imgMaxHeight
+            max-height 600px;
 
-        &-content {
-            text-align justify
+            h2 {
+                padding 2px;
+                margin 2px;
+            }
+            &-content {
+                height auto
+                text-align justify
+            }
+
         }
     }
 </style>
