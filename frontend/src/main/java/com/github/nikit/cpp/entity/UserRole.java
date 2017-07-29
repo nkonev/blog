6eb08459802160bda56bd12f3b864a5503a9addc
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-07-29 17:41:41 +0300

    comment

--- frontend/src/main/java/com/github/nikit/cpp/entity/UserRole.java
+++ frontend/src/main/java/com/github/nikit/cpp/entity/UserRole.java
@@ -1,6 +1,7 @@
 package com.github.nikit.cpp.entity;
 
 public enum UserRole {
+    // You shouldn't to change order of enum entries because these used in Hibernate's @Enumerated
     ROLE_ADMIN,
     ROLE_USER
 }
