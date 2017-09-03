49990bae5355e6c618a7023ac0c9dea4d8f3c872
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-09-03 07:48:24 +0300

    initial Spring Session

--- frontend/src/main/java/com/github/nikit/cpp/config/RedisConfig.java
+++ frontend/src/main/java/com/github/nikit/cpp/config/RedisConfig.java
@@ -6,13 +6,15 @@ import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.convert.WritingConverter;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 import javax.annotation.PostConstruct;
 import java.util.UUID;
 
 @Configuration
-@EnableRedisRepositories(basePackages="com.github.nikit.cpp.repo.redis")
+// https://stackoverflow.com/questions/41693774/spring-redis-indexes-not-deleted-after-main-entry-expires/41695902#41695902
+@EnableRedisRepositories(basePackages="com.github.nikit.cpp.repo.redis", enableKeyspaceEvents=RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
 }
--- frontend/src/main/java/com/github/nikit/cpp/config/WebSocketConfig.java
+++ frontend/src/main/java/com/github/nikit/cpp/config/WebSocketConfig.java
@@ -2,6 +2,8 @@ package com.github.nikit.cpp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -10,12 +12,13 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 // https://spring.io/guides/gs/messaging-stomp-websocket/
 // https://stackoverflow.com/questions/33977844/spring-send-message-to-websocket-clients
 // https://spring.io/blog/2015/10/13/react-js-and-spring-data-rest-part-4-events
+// https://docs.spring.io/spring-session/docs/1.3.1.RELEASE/reference/html5/guides/websocket.html
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession> {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void configureStompEndpoints(StompEndpointRegistry registry) {
         // the endpoint for websocket connections
         registry.addEndpoint("/stomp").setAllowedOrigins("*");
     }
