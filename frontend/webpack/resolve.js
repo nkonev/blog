1979d5942d7ada1c9498840813d8a0d1e5e16789
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2017-11-19 05:22:00 +0300

    initial coveraging

--- frontend/webpack/resolve.js
+++ frontend/webpack/resolve.js
@@ -5,5 +5,6 @@ module.exports = {
         // 'jquery': require.resolve('jquery'), // for uniform.js
         // 'vue': path.resolve(path.join(__dirname, 'node_modules', 'vue/dist/vue.js')), // fix "Vue is not constructor" in vue-online
         'vue$': path.resolve(path.join(__dirname, '..', 'node_modules', 'vue/dist/vue.esm.js')), // it's important, else you will get "You are using the runtime-only build of Vue where the template compiler is not available. Either pre-compile the templates into render functions, or use the compiler-included build."
+        //'@': path.resolve(__dirname, 'src')
     }
 }
