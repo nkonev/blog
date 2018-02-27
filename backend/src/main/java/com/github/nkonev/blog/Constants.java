9e4546b78b08ff07b51b66e1d97436e8d5d7778c
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2018-02-28 00:57:28 +0300

    removed unused constants

--- backend/src/main/java/com/github/nkonev/blog/Constants.java
+++ backend/src/main/java/com/github/nkonev/blog/Constants.java
@@ -5,11 +5,6 @@ package com.github.nkonev.blog;
  * Created by nik on 23.05.17.
  */
 public class Constants {
-    public static final String KEY = "autocomplete";
-    public static final String STOP_SYMBOL = "%";
-    public static final int STEP = 42;
-    public static final int COUNT = 50;
-    public static final String FRONTEND_RESOURCES = "${custom.frontend.resources}";
 
     public static class Uls {
         public static final String ROOT = "/";
