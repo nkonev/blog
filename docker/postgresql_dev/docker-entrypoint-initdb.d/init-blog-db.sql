1411e5e4a8df80b3812be3add8c3a8365ec0e365
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2018-05-14 03:29:08 +0300

    auto explain

--- docker/postgresql_dev/docker-entrypoint-initdb.d/init-blog-db.sql
+++ docker/postgresql_dev/docker-entrypoint-initdb.d/init-blog-db.sql
@@ -17,4 +17,22 @@ create database blog with owner blog;
 create extension if not exists "uuid-ossp" schema pg_catalog;
 -- create extension if not exists "hstore" schema pg_catalog;
 
+-- https://www.endpoint.com/blog/2012/10/30/postgresql-autoexplain-module
+-- ALTER SYSTEM set client_min_messages = notice;
+-- ALTER SYSTEM set log_min_messages = notice;
+-- ALTER SYSTEM set log_min_duration_statement = -1;
+-- ALTER SYSTEM set log_connections = on;
+-- ALTER SYSTEM set log_disconnections = on;
+-- ALTER SYSTEM set log_duration = on;
+
+--
+LOAD 'auto_explain';
+ALTER SYSTEM set shared_preload_libraries = 'auto_explain';
+ALTER SYSTEM SET auto_explain.log_min_duration=0;
+ALTER SYSTEM set auto_explain.log_analyze=true;
+ALTER SYSTEM set auto_explain.log_buffers=true;
+ALTER SYSTEM set auto_explain.log_timing=true;
+ALTER SYSTEM set auto_explain.log_verbose=true;
+ALTER SYSTEM set auto_explain.log_nested_statements=true;
+
 \connect blog blog;
