e636bb17fe5b17542aaea40a76a4c9f636210089
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2018-07-06 22:19:36 +0300

    output version to log

--- backend/src/main/java/com/github/nkonev/blog/config/AppConfig.java
+++ backend/src/main/java/com/github/nkonev/blog/config/AppConfig.java
@@ -6,15 +6,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.github.nkonev.blog.dto.UserAccountDetailsDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import javax.annotation.PostConstruct;
-import javax.persistence.EntityManagerFactory;
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 @Configuration
 public class AppConfig {
@@ -24,8 +29,10 @@ public class AppConfig {
     @Autowired
     private ServerProperties serverProperties;
 
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
+    @Value("classpath:/static/git.json")
+    private Resource resource;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
 
     @PostConstruct
     public void pc() throws Exception {
@@ -51,5 +58,13 @@ public class AppConfig {
         }
 
         return tomcat;
+    }
+
+    @PostConstruct
+    public void printVersion() throws IOException {
+        if(resource.exists()){
+            String text = new Scanner(resource.getInputStream()).useDelimiter("\\A").next();
+            LOGGER.info("Version {}", text);
+        }
     }
 }
