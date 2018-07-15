b6a5af4b2ce463aef0d7eceb9aa3df46b3453f69
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2018-07-15 18:19:05 +0300

    fix error in Firefox console on logout

--- backend/src/main/java/com/github/nkonev/blog/security/RESTAuthenticationLogoutSuccessHandler.java
+++ backend/src/main/java/com/github/nkonev/blog/security/RESTAuthenticationLogoutSuccessHandler.java
@@ -1,23 +1,31 @@
 package com.github.nkonev.blog.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 
 /**
  * Created by nik on 09.07.17.
  */
-
+@Component
 public class RESTAuthenticationLogoutSuccessHandler implements LogoutSuccessHandler {
 
     private final CsrfTokenRepository csrfTokenRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     public RESTAuthenticationLogoutSuccessHandler(CsrfTokenRepository csrfTokenRepository) {
         Assert.notNull(csrfTokenRepository, "csrfTokenRepository cannot be null");
@@ -32,5 +40,8 @@ public class RESTAuthenticationLogoutSuccessHandler implements LogoutSuccessHand
         // set new csrf token for repeating logins without page reload
         CsrfToken csrfToken = csrfTokenRepository.generateToken(request);
         this.csrfTokenRepository.saveToken(csrfToken, request, response);
+
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        objectMapper.writeValue(response.getWriter(), Collections.singletonMap("message", "you successfully logged out"));
     }
 }
