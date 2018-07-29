451de8132f37d425359720c7d128e80a122b26f5
Никита Конев (home Fedora) <nikit.cpp@yandex.ru>
2018-07-29 03:14:02 +0300

    savepoint

--- frontend/src/components/PostView.vue
+++ frontend/src/components/PostView.vue
@@ -19,6 +19,64 @@
                             <img class="edit-container-pen" src="../assets/pen.png" v-if="postDTO.canEdit" @click="setEdit()"/>
                             <img class="remove-container-x" src="../assets/remove.png" v-if="postDTO.canDelete" @click="openDeleteConfirmation()"/>
                         </div>
+
+
+                        <social-sharing url="https://vuejs.org/"
+                                        title="The Progressive JavaScript Framework"
+                                        description="Intuitive, Fast and Composable MVVM for building interactive interfaces."
+                                        quote="Vue is a progressive framework for building user interfaces."
+                                        hashtags="vuejs,javascript,framework"
+                                        twitter-user="vuejs"
+                                        inline-template>
+                            <div>
+                                <network network="email">
+                                    <i class="fa fa-envelope"></i> Email
+                                </network>
+                                <network network="facebook">
+                                    <i class="fa fa-facebook"></i> Facebook
+                                </network>
+                                <network network="googleplus">
+                                    <i class="fa fa-google-plus"></i> Google +
+                                </network>
+                                <network network="line">
+                                    <i class="fa fa-line"></i> Line
+                                </network>
+                                <network network="linkedin">
+                                    <i class="fa fa-linkedin"></i> LinkedIn
+                                </network>
+                                <network network="odnoklassniki">
+                                    <i class="fa fa-odnoklassniki"></i> Odnoklassniki
+                                </network>
+                                <network network="pinterest">
+                                    <i class="fa fa-pinterest"></i> Pinterest
+                                </network>
+                                <network network="reddit">
+                                    <i class="fa fa-reddit"></i> Reddit
+                                </network>
+                                <network network="skype">
+                                    <i class="fa fa-skype"></i> Skype
+                                </network>
+                                <network network="sms">
+                                    <i class="fa fa-commenting-o"></i> SMS
+                                </network>
+                                <network network="telegram">
+                                    <i class="fa fa-telegram"></i> Telegram
+                                </network>
+                                <network network="twitter">
+                                    <i class="fa fa-twitter"></i> Twitter
+                                </network>
+                                <network network="vk">
+                                    <i class="fa fa-vk"></i> VKontakte
+                                </network>
+                                <network network="weibo">
+                                    <i class="fa fa-weibo"></i> Weibo
+                                </network>
+                                <network network="whatsapp">
+                                    <i class="fa fa-whatsapp"></i> Whatsapp
+                                </network>
+                            </div>
+                        </social-sharing>
+
                     </div>
                     <div class="ql-container ql-bubble">
                         <div class="post-content ql-editor" v-html="postDTO.text"></div>
@@ -58,6 +116,7 @@
     import CommentList from './CommentList.vue'
     import Owner from './Owner.vue'
     import {getPostId, getTimestampFromUtc} from '../utils'
+    import SocialSharing from 'vue-social-sharing'
     // Lazy load heavy component https://router.vuejs.org/en/advanced/lazy-loading.html. see also in .babelrc
     const PostEdit = () => import('./PostEdit.vue');
 
@@ -93,6 +152,7 @@
             PostAddFab,
             CommentList,
             Owner,
+            SocialSharing
         },
         computed:{
             createTime(){
