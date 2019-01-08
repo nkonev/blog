c123c72a41d37177fd9ba67ffa13a0325aa7e188
Nikita Konev <nikit.cpp@yandex.ru>
2019-01-08 07:44:37 +0300

    clean html tags for websocket

--- backend/src/main/java/com/github/nkonev/blog/services/WebSocketService.java
+++ backend/src/main/java/com/github/nkonev/blog/services/WebSocketService.java
@@ -22,18 +22,22 @@ public class WebSocketService {
         this.objectMapper = objectMapper;
     }
 
-    private PostDTO deepCopy(PostDTO postDTO) throws IOException {
-        String s = objectMapper.writeValueAsString(postDTO);
-        return objectMapper.readValue(s, PostDTO.class);
+    private PostDTO deepCopy(PostDTO postDTO) {
+        try {
+            String s = objectMapper.writeValueAsString(postDTO);
+            return objectMapper.readValue(s, PostDTO.class);
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 
-    public void sendInsertPostEvent(PostDTO postDTO) throws IOException {
+    public void sendInsertPostEvent(PostDTO postDTO) {
         PostDTO copy = deepCopy(postDTO);
         copy.setText(cleanHtmlTags(postDTO.getText()));
         template.convertAndSend("/topic/posts.insert", copy);
     }
 
-    public void sendUpdatePostEvent(PostDTO postDTO) throws IOException {
+    public void sendUpdatePostEvent(PostDTO postDTO) {
         PostDTO copy = deepCopy(postDTO);
         copy.setText(cleanHtmlTags(postDTO.getText()));
         template.convertAndSend("/topic/posts.update", copy);
