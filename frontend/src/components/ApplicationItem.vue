0e52eaa0294d3feec4aadf86a73bc91c5837d115
Nikita Konev <nikit.cpp@yandex.ru>
2019-12-06 01:47:11 +0300

    sending login / logout events to iframe

--- frontend/src/components/ApplicationItem.vue
+++ frontend/src/components/ApplicationItem.vue
@@ -2,14 +2,37 @@
     <div class="application">
         <h2 :id="this.application.id"><a :href="'#'+this.application.id">#</a> {{this.application.title}}</h2>
         <div class="application-container" >
-            <iframe scrolling="no" :src="this.application.srcUrl"/>
+            <iframe :id="getId()" scrolling="no" :src="this.application.srcUrl"/>
         </div>
     </div>
 </template>
 
 <script>
+    import bus, {LOGIN, LOGOUT} from '../bus'
+
     export default {
         props: ['application'],
+        methods: {
+            getId(){
+                return 'ifr_'+this.$props.application.id;
+            },
+            onSuccessLogin() {
+                const iframeEl = document.getElementById(this.getId());
+                iframeEl.contentWindow.postMessage(LOGIN, '*');
+            },
+            onSuccessLogout() {
+                const iframeEl = document.getElementById(this.getId());
+                iframeEl.contentWindow.postMessage(LOGOUT, '*');
+            },
+        },
+        created(){
+            bus.$on(LOGIN, this.onSuccessLogin);
+            bus.$on(LOGOUT, this.onSuccessLogout);
+        },
+        destroyed() {
+            bus.$off(LOGIN, this.onSuccessLogin);
+            bus.$off(LOGOUT, this.onSuccessLogout);
+        },
     }
 </script>
 
