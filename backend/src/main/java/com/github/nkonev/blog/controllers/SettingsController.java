63eef2cc756e99dd6b319c0ca2a06c8669bf8cf4
Nikita Konev <nikit.cpp@yandex.ru>
2019-12-14 06:15:05 +0300

    optimize SettingsController

--- backend/src/main/java/com/github/nkonev/blog/controllers/SettingsController.java
+++ backend/src/main/java/com/github/nkonev/blog/controllers/SettingsController.java
@@ -1,23 +1,31 @@
 package com.github.nkonev.blog.controllers;
 
-import com.github.nkonev.blog.config.ApplicationConfig;
 import com.github.nkonev.blog.dto.SettingsDTO;
 import com.github.nkonev.blog.dto.UserAccountDetailsDTO;
 import com.github.nkonev.blog.dto.UserRole;
+import com.github.nkonev.blog.entity.jpa.RuntimeSettings;
 import com.github.nkonev.blog.repo.jpa.RuntimeSettingsRepository;
 import com.github.nkonev.blog.security.BlogSecurityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
 
-import static com.github.nkonev.blog.Constants.Urls.*;
+import static com.github.nkonev.blog.Constants.Urls.API;
+import static com.github.nkonev.blog.Constants.Urls.CONFIG;
 
 @RestController
 public class SettingsController {
@@ -44,25 +52,51 @@ public class SettingsController {
     private static final String TITLE_TEMPLATE = "title.template";
     private static final String BACKGROUND_COLOR = "background.color";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SettingsController.class);
+
     @GetMapping(API+CONFIG)
     public SettingsDTO getConfig(@AuthenticationPrincipal UserAccountDetailsDTO userAccount){
-        String imageBackground = runtimeSettingsRepository.findById(IMAGE_BACKGROUND).orElseThrow().getValue();
-        String header = runtimeSettingsRepository.findById(HEADER).orElseThrow().getValue();
-        String subHeader = runtimeSettingsRepository.findById(SUB_HEADER).orElseThrow().getValue();
-        String titleTemplate = runtimeSettingsRepository.findById(TITLE_TEMPLATE).orElseThrow().getValue();
-        String backgroundColor = runtimeSettingsRepository.findById(BACKGROUND_COLOR).orElseThrow().getValue();
+
+        Collection<RuntimeSettings> runtimeSettings = runtimeSettingsRepository
+                .findByKeyIn(Set.of(IMAGE_BACKGROUND, HEADER, SUB_HEADER, TITLE_TEMPLATE, BACKGROUND_COLOR));
+
+        SettingsDTO settingsDTOPartial = runtimeSettings.stream()
+                .reduce(new SettingsDTO(), (
+                        settingsDTO, runtimeSettings1) -> {
+                            if (runtimeSettings1.getKey() == null) {
+                                throw new RuntimeException("Null key is not supported");
+                            }
+                            switch (runtimeSettings1.getKey()) {
+                                case IMAGE_BACKGROUND:
+                                    settingsDTO.setImageBackground(runtimeSettings1.getValue());
+                                    break;
+                                case HEADER:
+                                    settingsDTO.setHeader(runtimeSettings1.getValue());
+                                    break;
+                                case SUB_HEADER:
+                                    settingsDTO.setSubHeader(runtimeSettings1.getValue());
+                                    break;
+                                case TITLE_TEMPLATE:
+                                    settingsDTO.setTitleTemplate(runtimeSettings1.getValue());
+                                    break;
+                                case BACKGROUND_COLOR:
+                                    settingsDTO.setBackgroundColor(runtimeSettings1.getValue());
+                                    break;
+                                default:
+                                    LOGGER.warn("Unknown key " + runtimeSettings1.getKey());
+                            }
+                            return settingsDTO;
+                        },
+                        (settingsDTO, settingsDTO2) -> { throw new UnsupportedOperationException("Parallel is not supported");}
+        );
 
         boolean canShowSettings = blogSecurityService.hasSettingsPermission(userAccount);
-        return new SettingsDTO(
-                header,
-                subHeader,
-                titleTemplate,
-                imageBackground,
-                backgroundColor,
-                canShowSettings,
-                applicationConfig.isEnableApplications(),
-                Arrays.asList(UserRole.ROLE_ADMIN, UserRole.ROLE_USER)
-        );
+
+        settingsDTOPartial.setCanShowSettings(canShowSettings);
+        settingsDTOPartial.setCanShowApplications(applicationConfig.isEnableApplications());
+        settingsDTOPartial.setAvailableRoles(Arrays.asList(UserRole.ROLE_ADMIN, UserRole.ROLE_USER));
+
+        return settingsDTOPartial;
     }
 
     @Transactional
--- backend/src/main/java/com/github/nkonev/blog/repo/jpa/RuntimeSettingsRepository.java
+++ backend/src/main/java/com/github/nkonev/blog/repo/jpa/RuntimeSettingsRepository.java
@@ -4,7 +4,10 @@ import com.github.nkonev.blog.entity.jpa.RuntimeSettings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.Set;
+
 @Repository
 public interface RuntimeSettingsRepository extends JpaRepository<RuntimeSettings, String> {
-
+    Collection<RuntimeSettings> findByKeyIn(Set<String> keys);
 }
