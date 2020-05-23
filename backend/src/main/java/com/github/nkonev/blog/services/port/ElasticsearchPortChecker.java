e3eebcd7e13d98d6d58e288d8546a844efc7796a
Nikita Konev <nikit.cpp@yandex.ru>
2020-05-23 07:54:52 +0300

    move to Spring Boot 2.3.0.RELEASE

--- backend/src/main/java/com/github/nkonev/blog/services/port/ElasticsearchPortChecker.java
+++ backend/src/main/java/com/github/nkonev/blog/services/port/ElasticsearchPortChecker.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientProperties;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -19,7 +19,7 @@ public class ElasticsearchPortChecker extends AbstractPortChecker {
     private int maxCount;
 
     @Autowired
-    private RestClientProperties elasticsearchProperties;
+    private ElasticsearchRestClientProperties elasticsearchProperties;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ElasticsearchPortChecker.class);
 
